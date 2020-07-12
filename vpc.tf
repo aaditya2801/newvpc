e06739c06d05fc1ac54369832e44dd9b9d5e3794
aaditya2801 <66722117+aaditya2801@users.noreply.github.com>
2020-07-12 19:36:33 +0530

    Update vpc.tf

--- vpc.tf
+++ vpc.tf
@@ -213,7 +213,7 @@ resource "aws_instance" "mysql" {
   ami           = "ami-0019ac6129392a0f2"
   instance_type = "t2.micro"
   subnet_id = aws_subnet.private.id
-  vpc_security_group_ids = ["${aws_security_group.mysqlsecurity.id}","${aws_security_group.mysqlserversecurity.id}"]
+  vpc_security_group_ids = ["${aws_security_group.mysqlsecurity.id}","${aws_security_group.mybastionsecurity.id}"]
   key_name = "mynewkey"
   availability_zone = "ap-south-1b"
 
@@ -228,7 +228,7 @@ resource "aws_instance" "bastionhost" {
   associate_public_ip_address = true
   subnet_id = aws_subnet.public.id
   vpc_security_group_ids = ["${aws_security_group.mybastionsecurity.id}"]
-  key_name = "mynewkey"
+  key_name = "newkey"
   availability_zone = "ap-south-1a"
 
   tags = {
